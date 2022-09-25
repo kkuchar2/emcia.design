900f89178ba0c87a7bd82e43d7dfd65c334850c4
Krzysztof Kucharski <krzysiekkucharski7@gmail.com>
2022-09-25 19:33:27 +0200

    Disco

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,10 +23,85 @@ const StyledText3 = styled.span`
 `;
 
 const Dot = styled.div`
-    width: 100px;
-    height: 100px;
-    background: #1e1e1e;
+
+  @keyframes flip_horizontal {
+    0% {
+      transform: rotateY(0deg);
+      background-position: unset;
+    }
+    24% {
+      transform: rotateY(0deg);
+      background-position: unset;
+    }
+    25% {
+      transform: rotateY(180deg);
+      background-position: center;
+    }
+    49% {
+      transform: rotateY(180deg);
+      background-position: center;
+    }
+    50% {
+      transform: rotateY(0deg);
+      background-position: center;
+    }
+    74% {
+      transform: rotateY(0deg);
+      background-position: center;
+    }
+    75% {
+      transform: rotateY(180deg);
+      background-position: unset;
+    }
+    100% {
+      transform: rotateY(180deg);
+      background-position: unset;
+    }
+  }
+
+  width: 200px;
+  height: 200px;
+  background: url('/images/emcia.jpg');
+  background-size: cover;
+  border-radius: 50%;
+  animation: flip_horizontal 4s infinite;
+
+  @keyframes disco {
+    0% {
+      background: rgba(255, 0, 0, 0.2);
+    }
+    24% {
+      background: rgba(255, 0, 0, 0.2);
+    }
+    25% {
+      background: rgba(255, 255, 0, 0.2);
+    }
+    49% {
+      background: rgba(255, 255, 0, 0.2);
+    }
+    50% {
+      background: rgba(0, 255, 18, 0.2);
+    }
+    74% {
+      background: rgba(0, 255, 18, 0.2);
+    }
+    75% {
+      background: rgba(0, 0, 255, 0.2);
+    }
+    100% {
+      background: rgba(0, 0, 255, 0.2);
+    }
+  }
+
+  &:after {
+    content: '';
+    display: block;
+    width: 200px;
+    height: 200px;
+    background: rgba(255, 0, 0, 0.09);
     border-radius: 50%;
+    animation: disco 1s infinite;
+  }
 `;
 
 const Index = () => {
@@ -37,7 +112,7 @@ const Index = () => {
         {/*<NavBar/>*/}
         <div className={'w-full max-w-[3000px] mx-auto bg-red relative overflow-hidden'}>
             <StyledHome>
-                <Dot />
+                <Dot/>
                 {/*<CenterBox>*/}
                 {/*    <div className={'animate-fadein1 absolute top-0 right-[40px] w-[333px] h-[1px] bg-black/30'}/>*/}
                 {/*    <div className={'animate-fadein2 absolute bottom-0 left-[40px] w-[333px] h-[1px] bg-black'}/>*/}
