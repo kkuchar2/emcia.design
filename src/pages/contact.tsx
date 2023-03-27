e4727f8858c5989ce9144f6b98e8ebba78df0a1d
Krzysztof Kucharski <krzysiekkucharski7@gmail.com>
2023-03-27 11:03:40 +0200

    Simplify contact page css

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -7,80 +7,40 @@ import styled from 'styled-components';
 
 const StyledContact = styled.div`
   position: relative;
-  top: 0;
-  width: 100%;
   display: flex;
   flex-direction: column;
 
   @media (min-width: 1024px) {
     align-items: center;
-    height: 100vh;
+    min-height: 100vh;
   }
 `;
 
 const LeftSide = styled.div`
   display: none;
-  align-items: center;
-  justify-content: center;
-  z-index: 2;
-  width: 0;
-  overflow: visible;
 
   @media (min-width: 1024px) {
-    display: flex;
-    width: 50%;
-    opacity: 1;
-    height: 100%;
+    display: block;
+    width: calc(50%);
   }
 `;
 
 const RightSide = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  display: grid;
+  place-items: center;
   padding: 30px;
-  width: 100%;
+  flex-grow: 1;
 
   @media (min-width: 1024px) {
     width: 50%;
   }
 `;
 
-const Wrapper = styled.div`
-  position: relative;
-  width: min(100%, 600px);
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-
-  @media (min-width: 1024px) {
-    margin-top: 0;
-    padding-bottom: 0;
-  }
-`;
-
-const StyledMobileTitle = styled.div`
-  font-size: 2.8rem;
-  visibility: visible;
-  font-weight: 600;
-  color: #1E1E1E;
-  z-index: 4;
-  text-align: center;
-
-  @media (min-width: 1024px) {
-    display: none;
-  }
-`;
-
-const StyledDesktopTitlePart = styled.div`
+const BigScreenTitle = styled.div`
   position: fixed;
   top: 50%;
   right: calc(50% + 200px);
   mix-blend-mode: difference;
-
-  z-index: 4;
-
   display: none;
   color: #ffffff;
 
@@ -93,19 +53,28 @@ const StyledDesktopTitlePart = styled.div`
   }
 `;
 
-const StyledDesktopTitle = styled.div`
-  font-size: clamp(2.8rem, 6vw, 5rem);
+const Title = styled.div`
+  font-size: 2.8rem;
+  visibility: visible;
   font-weight: 600;
-`;
-
-const StyledDesktopTitleDescription = styled.div`
-  font-size: clamp(1rem, 2vw, 1rem);
-  font-weight: 400;
-  color: #898989;
-  display: none;
+  color: white;
+  mix-blend-mode: difference;
+  text-align: center;
 
   @media (min-width: 1024px) {
-    display: block;
+    font-size: clamp(2.8rem, 6vw, 5rem);
+    font-weight: 600;
+  }
+`;
+
+const TitleHelloMessage = styled.div`
+  font-size: clamp(1rem, 2vw, 1rem);
+  font-weight: 400;
+  color: #807F7F;
+  text-align: center;
+
+  @media (min-width: 1024px) {
+    text-align: left;
   }
 `;
 
@@ -117,7 +86,6 @@ const TopCircle = styled.div`
   height: 100vw;
   top: calc(-50vw + 100px);
   left: 0;
-  z-index: 1;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -150,7 +118,7 @@ const FakeNavbar = styled.div`
   }
 `;
 
-const FakeCirclePart = styled.div`
+const FakeCircleBlock = styled.div`
   width: 100%;
   height: calc(50vw);
 
@@ -167,13 +135,13 @@ const FakeCirclePart = styled.div`
   }
 `;
 
-const DesktopCircle = styled.div`
+const BigScreenCircle = styled.div`
   position: absolute;
   width: 160vh;
   height: 160vh;
   border-radius: 50%;
   background: #ffffff;
-  right: calc(50% + 80px);
+  right: calc(50%);
   bottom: calc(50% - 80vh);
   display: none;
 
@@ -181,6 +149,7 @@ const DesktopCircle = styled.div`
     width: 100vw;
     height: 100vw;
     bottom: calc(50% - 70vw);
+    right: calc(50% + 80px);
   }
 
   @media (min-width: 1024px) {
@@ -189,39 +158,51 @@ const DesktopCircle = styled.div`
   }
 `;
 
+const FormAndSocialMedia = styled.div`
+  width: min(100%, 600px);
+`;
+
 export default function Contact() {
 
     return <StyledContact>
+
         <div className={'fixed top-0 left-0 z-0 h-screen w-screen'}>
-            <DesktopCircle/>
+            <BigScreenCircle/>
         </div>
-        <StyledDesktopTitlePart>
-            <StyledDesktopTitle>{'contact'}</StyledDesktopTitle>
-            <StyledDesktopTitleDescription>{'Would you like to work with me?'}</StyledDesktopTitleDescription>
-            <StyledDesktopTitleDescription>{'Send a message!'}</StyledDesktopTitleDescription>
-        </StyledDesktopTitlePart>
+
+        <BigScreenTitle>
+            <Title>{'contact'}</Title>
+            <div>
+                <TitleHelloMessage>{'Would you like to work with me?'}</TitleHelloMessage>
+                <TitleHelloMessage>{'Send a message!'}</TitleHelloMessage>
+            </div>
+        </BigScreenTitle>
+
         <FakeNavbar/>
-        <FakeCirclePart/>
+
+        <FakeCircleBlock/>
 
         <TopCircle>
             <div className={'absolute bottom-[50px] flex flex-col gap-7'}>
-                <StyledMobileTitle>{'contact'}</StyledMobileTitle>
-                <div className={'flex flex-col gap-1 text-[#807F7F]'}>
-                    <div className={'flex w-full justify-center xl:justify-start'}>{'Would you like to work with me?'}</div>
-                    <div className={'flex w-full justify-center xl:justify-start'}>{'Send a message!'}</div>
+                <Title>{'contact'}</Title>
+                <div>
+                    <TitleHelloMessage>{'Would you like to work with me?'}</TitleHelloMessage>
+                    <TitleHelloMessage>{'Send a message!'}</TitleHelloMessage>
                 </div>
             </div>
         </TopCircle>
+
         <div className={'flex w-full grow pt-7 md:pt-0'}>
             <LeftSide/>
             <RightSide>
-                <Wrapper>
+                <FormAndSocialMedia>
                     <ContactForm/>
                     <div className={'mt-[20px] flex w-full justify-center sm:mt-[50px] md:mt-[50px]'}>
                         <SocialMedia title={'or check out my social media'}/>
                     </div>
-                </Wrapper>
+                </FormAndSocialMedia>
             </RightSide>
         </div>
+
     </StyledContact>;
 }
