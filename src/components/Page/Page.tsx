4d490bc50105df5907b8ada7dbfe9b05b00962e5
Krzysztof Kucharski <krzysiekkucharski7@gmail.com>
2023-03-27 02:16:14 +0200

    Clean up old scroll listener from Page.tsx

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -2,20 +2,6 @@ import React from 'react';
 
 import { NavBar } from 'components/NavBar/NavBar';
 
-const listener = (status) => {
-    const remaining = status.limit.y - status.offset.y;
-    const fullScroll = status.limit.y;
-    const scale = remaining / fullScroll;
-    document.documentElement.style.setProperty('--scale-bottom-circle', `${scale}`);
-
-    // Set var with scroll position:
-    document.documentElement.style.setProperty('--scroll-position', `${status.offset.y}px`);
-
-    // Set var with scroll progress:
-    const scrollProgress = status.offset.y / status.limit.y;
-    document.documentElement.style.setProperty('--scroll-progress', `${scrollProgress}`);
-};
-
 interface PageProps {
     component: React.FC;
     pageProps: any;
@@ -24,25 +10,6 @@ interface PageProps {
 export const Page = (props: PageProps) => {
 
     const { component: Component, pageProps } = props;
-
-    React.useEffect(() => {
-        window.addEventListener('scroll', () => {
-            listener({
-                offset: {
-                    x: window.scrollX,
-                    y: window.scrollY
-                },
-                limit: {
-                    x: document.body.scrollWidth - window.innerWidth,
-                    y: document.body.scrollHeight - window.innerHeight
-                }
-            });
-        });
-
-        return () => {
-            window.removeEventListener('scroll', listener);
-        };
-    }, []);
 
     return <div className={'w-full'}>
         <NavBar/>
