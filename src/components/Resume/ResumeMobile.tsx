fe1b5e4e3e07422f27b3974ed48f6077eee78b10
Krzysztof Kucharski <krzysiekkucharski7@gmail.com>
2025-10-05 11:59:51 +0200

    Update year in MadeBy component and adjust styles in ResumeMobile component for consistency

--- src/components/Resume/ResumeMobile.tsx
+++ src/components/Resume/ResumeMobile.tsx
@@ -1,72 +1,72 @@
 import React from 'react';
 
-import { BottomResumeSection } from 'components/BottomResumeSection/BottomResumeSection';
-import { Education } from 'components/Resume/Education';
-import { Experience } from 'components/Resume/Experience';
+import {BottomResumeSection} from 'components/BottomResumeSection/BottomResumeSection';
+import {Education} from 'components/Resume/Education';
+import {Experience} from 'components/Resume/Experience';
 import styled from 'styled-components';
 
 const StyledResume = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  container-type: inline-size;
-  letter-spacing: 0.03em;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    container-type: inline-size;
+    letter-spacing: 0.03em;
 `;
 
 const Title = styled.div`
-  font-size: 2.8rem;
-  visibility: visible;
-  font-weight: 600;
-  color: #e1e1e1;
-  text-align: center;
-  position: relative;
+    font-size: 2.8rem;
+    visibility: visible;
+    font-weight: 600;
+    color: #e1e1e1;
+    text-align: center;
+    position: relative;
 `;
 
 const TitleHelloMessage = styled.div`
-  font-size: clamp(1rem, 1vw, 1rem);
-  font-weight: 400;
-  color: #807F7F;
-  text-align: center;
+    font-size: clamp(1rem, 1vw, 1rem);
+    font-weight: 400;
+    color: #807F7F;
+    text-align: center;
 `;
 
 const TopCircle = styled.div`
-  position: absolute;
-  background: #f1f1f1;
-  border-bottom-left-radius: 50%;
-  border-bottom-right-radius: 50%;
-  width: 100vw;
-  height: 100vw;
-  top: calc(-50vw + 100px);
-  left: 0;
-  align-items: center;
-  justify-content: center;
-  display: flex;
+    position: absolute;
+    background: #f1f1f1;
+    border-bottom-left-radius: 50%;
+    border-bottom-right-radius: 50%;
+    width: 100%;
+    height: 100vw;
+    top: calc(-50vw + 100px);
+    left: 0;
+    align-items: center;
+    justify-content: center;
+    display: flex;
 
-  @media (min-width: 640px) {
-    width: 100vw;
-    height: 80vw;
-    top: calc(100px - 40vw);
-  }
+    @media (min-width: 640px) {
+        width: 100%;
+        height: 80vw;
+        top: calc(100px - 40vw);
+    }
 
-  @media (min-width: 768px) {
-    width: 100vw;
-    height: 60vw;
-    top: calc(100px - 30vw);
-  }
+    @media (min-width: 768px) {
+        width: 100%;
+        height: 60vw;
+        top: calc(100px - 30vw);
+    }
 `;
 
 const FakeCircleBlock = styled.div`
-  width: 100%;
-  height: calc(50vw);
-  background: #1e1e1e;
+    width: 100%;
+    height: calc(50vw);
+    background: #1e1e1e;
 
-  @media (min-width: 640px) {
-    height: 40vw;
-  }
+    @media (min-width: 640px) {
+        height: 40vw;
+    }
 
-  @media (min-width: 768px) {
-    height: 30vw;
-  }
+    @media (min-width: 768px) {
+        height: 30vw;
+    }
 `;
 
 export const ResumeMobile = () => {
@@ -80,7 +80,7 @@ export const ResumeMobile = () => {
                 </div>
             </div>
         </TopCircle>
-        <div className={'h-[100px] w-full bg-[#f1f1f1]'}/>
+        <div className={'opacity-0 sm:visible h-[100px] w-full bg-red-500'}/>
         <FakeCircleBlock/>
         <div className={'flex flex-col items-center justify-center bg-[#f1f1f1]'}>
             <Experience/>
